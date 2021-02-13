78b33412bacd26b49db4f5ed9c6673f2e8bf981c
Jorge <jorgesantamariapoveda@gmail.com>
2021-02-13 18:12:39 +0100

    initial commit

---
+++ XCTestUse/XCTestUse/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  XCTestUse
+//
+//  Created by Jorge on 12/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
